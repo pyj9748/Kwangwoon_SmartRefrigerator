5a4c2dfc82aee2bc1da3cd4fb1648757a0ffa0ff
pyj9748 <pyj9748@naver.com>
2020-11-29 20:59:22 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 
-
 #include "cook.h"
 #include "ingredient.h"
 #include "recipe.h"
 #include "show.h"
+#include <iostream>
 
-int main() { return 0; }
+using namespace std;
+list<Ingredient> ingredient_list;
+list<Recipe> recipe_list;
+list<Ingredient>::iterator it_ing;
+list<Recipe>::iterator it_rec;
+int main() {
+
+    show_main();
+
+    return 0;
+}
