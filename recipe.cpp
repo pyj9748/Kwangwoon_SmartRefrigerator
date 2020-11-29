69ae05323a0e022f66f8b3d9e1e2395e1ef08781
pyj9748 <pyj9748@naver.com>
2020-11-29 20:58:30 +0900

    recipe.cpp

--- recipe.cpp
+++ recipe.cpp
@@ -1,20 +1,10 @@
 #include "recipe.h"
 #include <list>
 #include <string>
-Recipe::Recipe(string name, string content, Ingredient ingredient,
-               string difficulty, int time) {
-    set_recipe_name(name);
-    set_recipe_content(content);
-    set_recipe_ingredient(ingredient);
-    set_recipe_difficulty(difficulty);
-    set_recipe_time(time);
-}
 
 string Recipe::get_recipe_name() { return recipe_name; }
 string Recipe::get_recipe_content() { return recipe_content; }
-list<Ingredient> Recipe::get_recipe_ingredient() {
-    return recipe_ingredient_list;
-}
+list<string> Recipe::get_recipe_ingredient() { return recipe_ingredient_list; }
 string Recipe::get_recipe_difficulty() { return recipe_difficulty; }
 int Recipe::get_recipe_time() { return recipe_time; }
 
@@ -22,9 +12,9 @@ void Recipe::set_recipe_name(string name) { this->recipe_name = name; }
 void Recipe::set_recipe_content(string content) {
     this->recipe_content += content;
 }
-void Recipe::set_recipe_ingredient(Ingredient ingredient) {
+void Recipe::set_recipe_ingredient(list<string> ingredient) {
 
-    this->recipe_ingredient_list.push_back(ingredient);
+    this->recipe_ingredient_list.assign(ingredient.begin(), ingredient.end());
 }
 
 void Recipe::set_recipe_difficulty(string difficulty) {
