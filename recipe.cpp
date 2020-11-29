733cdd78d58e4fe6c5950ee97df3537f476c9aa8
pyj9748 <pyj9748@naver.com>
2020-11-29 12:24:36 +0900

    recipe.cpp

--- recipe.cpp
+++ recipe.cpp
@@ -1,6 +1,7 @@
 #include "recipe.h"
-
-Recipe::Recipe(string name, string content, string ingredient,
+#include <list>
+#include <string>
+Recipe::Recipe(string name, string content, Ingredient ingredient,
                string difficulty, int time) {
     set_recipe_name(name);
     set_recipe_content(content);
@@ -11,16 +12,22 @@ Recipe::Recipe(string name, string content, string ingredient,
 
 string Recipe::get_recipe_name() { return recipe_name; }
 string Recipe::get_recipe_content() { return recipe_content; }
-string Recipe::get_recipe_ingredient() { return recipe_ingredient; }
+list<Ingredient> Recipe::get_recipe_ingredient() {
+    return recipe_ingredient_list;
+}
 string Recipe::get_recipe_difficulty() { return recipe_difficulty; }
 int Recipe::get_recipe_time() { return recipe_time; }
 
-void Recipe::set_recipe_name(string name) { recipe_name = name; }
-void Recipe::set_recipe_content(string content) { recipe_content = content; }
-void Recipe::set_recipe_ingredient(string ingredient) {
-    recipe_ingredient = ingredient;
+void Recipe::set_recipe_name(string name) { this->recipe_name = name; }
+void Recipe::set_recipe_content(string content) {
+    this->recipe_content += content;
 }
+void Recipe::set_recipe_ingredient(Ingredient ingredient) {
+
+    this->recipe_ingredient_list.push_back(ingredient);
+}
+
 void Recipe::set_recipe_difficulty(string difficulty) {
-    recipe_difficulty = difficulty;
+    this->recipe_difficulty = difficulty;
 }
-void Recipe::set_recipe_time(int time) { recipe_time = time; }
+void Recipe::set_recipe_time(int time) { this->recipe_time = time; }
