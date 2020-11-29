c6bc2fec89e337d0ff2e45a957087456a40ad080
pyj9748 <pyj9748@naver.com>
2020-11-29 12:01:15 +0900

    recipe.h

--- recipe.h
+++ recipe.h
@@ -1,4 +1,31 @@
-#ifndef RECIPE_H
-#define RECIPE_H
+#include "ingredient.h"
+#include <iostream>
+#include <list>
+#include <string>
 
-#endif
+using namespace std;
+
+class Recipe {
+  public:
+    Recipe(string name, string content, string ingredient, string difficulty,
+           int time);
+
+    string get_recipe_name();
+    string get_recipe_content();
+    string get_recipe_ingredient();
+    string get_recipe_difficulty();
+    int get_recipe_time();
+
+    void set_recipe_name(string str);
+    void set_recipe_content(string str);
+    void set_recipe_ingredient(string str);
+    void set_recipe_difficulty(string str);
+    void set_recipe_time(int n);
+
+  private:
+    string recipe_name;                      //이름
+    string recipe_content[];                 //내용
+    list<Ingredient> recipe_ingredient_list; // 재료 (linked list)
+    string recipe_difficulty;                // 난이도
+    int recipe_time;                         //조리 시간
+};
