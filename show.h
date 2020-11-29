7253083756116b1f959218bf8bceda134f5085fd
pyj9748 <pyj9748@naver.com>
2020-11-29 20:59:05 +0900

    show.h

--- show.h
+++ show.h
@@ -1,4 +1,13 @@
-#ifndef SHOW_H
-#define SHOW_H
+#include "ingredient.h"
+#include "recipe.h"
 
-#endif
+//메인 메뉴
+void show_main();
+// 선택한 메뉴
+void show_1();
+void show_2();
+void show_3();
+void show_4();
+void show_5();
+void show_6();
+void show_7();
