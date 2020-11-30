e149914b4c8842810417791dd32e3a5d948d4639
pyj9748 <pyj9748@naver.com>
2020-12-01 02:39:50 +0900

    show.hpp

---
+++ show.hpp
@@ -0,0 +1,17 @@
+#ifndef __SHOW_H__
+#define __SHOW_H__
+#include "ingredient.hpp"
+#include "recipe.hpp"
+
+//메인 메뉴
+void show_main();
+
+// 선택한 메뉴
+void show_1();
+void show_2();
+void show_3();
+void show_4();
+void show_5();
+void show_6();
+void show_7();
+#endif
