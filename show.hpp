3a394c7b17185cdac927f6d9c1afe465a0a091c5
babful <alstn6787@gmail.com>
2020-12-02 00:43:44 +0900

    요리하기 기능 완성

--- show.hpp
+++ show.hpp
@@ -17,6 +17,6 @@ void show_3(list<Recipe> &recipe_list);     // 레시피 추가
 void show_4(list<Recipe> &recipe_list);     // 레시피 삭제
 void show_5(list<ingred> &ingredient_list); // 재료 목록
 void show_6(list<Recipe> &recipe_list);     // 레시피 목록
-void show_7(const list<ingred> &ingredient_list,
+void show_7(list<ingred> &ingredient_list,
             const list<Recipe> &recipe_list); // 요리 하기
 #endif
